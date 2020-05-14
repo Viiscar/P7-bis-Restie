5bdf286a27f3a547eba8d661bdbe5bbfa620ac1f
FLR <fanchleroux@tutanota.com>
2020-05-14 10:30:04 -0400

    Updates

---
+++ precache-manifest.e2d85092fa68f4716d2f22f3f0f8abbf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27c727e9b9ab2db5ec553ffc510274bf",
+    "url": "/P7-bis-Restie/index.html"
+  },
+  {
+    "revision": "365b6b11017e2b50fd80",
+    "url": "/P7-bis-Restie/static/js/2.4d02d402.chunk.js"
+  },
+  {
+    "revision": "6cf08280aad963071c0409c5b1ec5617",
+    "url": "/P7-bis-Restie/static/js/2.4d02d402.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f136e129a9ed67e6919",
+    "url": "/P7-bis-Restie/static/js/main.43663a12.chunk.js"
+  },
+  {
+    "revision": "526e5f43f13259d40894",
+    "url": "/P7-bis-Restie/static/js/runtime-main.e0b25ffd.js"
+  }
+]);
